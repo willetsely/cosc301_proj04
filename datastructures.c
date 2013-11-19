d0f902a75d4fac70b4f188defa57eafe3e79b33b
willetsely <willestely@gmail.com>
2013-11-18 22:18:23 -0500

    datastructures.c created and includes request_t_insert and request_t_remove

---
+++ datastructures.c
@@ -0,0 +1,45 @@
+#include "main.c"
+//include a bunch of stuff
+
+//declare global variables
+extern request_t *head;
+extern request_t *tail;
+
+typedef struct request {
+    char *filename;
+    int socket;
+    //log entry
+    struct request *next;
+} request_t;
+
+request_t *request_t_insert(char *filename, int socket,)
+{
+    request_t *new = malloc(sizeof(request_t *));
+    if(new == NULL)
+        return NULL;
+    new->filename = filename;
+    new->socket = socket;
+    new->next = head;
+    // new->log entry = ? (do we need to malloc?)
+    return new;
+}
+
+request_t *request_t_remove()
+{
+    request_t *temp = head;
+    if(head != tail) //if more than one node left
+    {    
+        while(temp->next->next != NULL) //traverse the list until right before tail
+        {
+            temp = temp->next;
+        }
+        temp->next = NULL; //cut off the tail
+        free(tail); //free the tail
+    }
+    else    //if head == tail: only one request in the list
+    {
+        free(head);
+        temp = NULL;
+    }
+    return temp;
+}
