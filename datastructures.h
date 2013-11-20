db7993bcaddbb5b4392c23e6035e54ecb6e0eabb
willetsely <willestely@gmail.com>
2013-11-20 01:30:07 -0500

    Save my nonsense

--- datastructures.h
+++ datastructures.h
@@ -1,7 +1,14 @@
 #ifndef __DATASTRUCTURES_H__
 #define __DATASTRUCTURES_H__
 
-struct request_t {
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+
+struct request {
     char *filename;
     int socket;
     //log entry
